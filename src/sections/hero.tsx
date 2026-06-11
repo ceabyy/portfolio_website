9f2a661b3ebb20da63693b899b1d68744be7de51
ceabyy <ceabyfernandez@gmail.com>
2026-06-11 22:54:37 +1200

    added some adjustments for mobile temporarily

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -3,8 +3,8 @@
 function Hero() {
     return (
         <div className="h-dvh flex flex-col items-start justify-center snap-y pl-[15%]">
-            <h1 className="text-[40px] text-white"> CEABY KIAN FERNANDEZ </h1>
-            <h1 className="text-[40px] font-bold underline text-white text-underline -mt-5"> portfolio.</h1>
+            <h1 className="text-[20px] md:text-[40px] text-white"> CEABY KIAN FERNANDEZ </h1>
+            <h1 className="text-[20px] md:text-[40px] font-bold underline text-white text-underline md:-mt-5 "> portfolio.</h1>
         </div>
     )
 }
