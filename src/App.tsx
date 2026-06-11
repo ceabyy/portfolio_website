09a9bc623474a2bbb2445d4772bac4e300198e6e
ceabyy <ceabyfernandez@gmail.com>
2026-06-11 22:09:29 +1200

    adding files for vite project

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import './App.css'
+import Hero from './sections/hero.tsx'
+import About from './sections/about.tsx'
+import Experience from './sections/experience.tsx'
+import Projects from './sections/projects.tsx'
+import Education from './sections/education.tsx'
+import Links from './sections/links.tsx'
+
+
+function App() {
+
+  return (
+    // handling background of the page using gradients from figma prototype.
+    <div style={{
+      minHeight: '100vh',
+      background: `linear-gradient(to bottom,
+        #FFE398 0%,
+        #FF8AAB 4%,
+        #FFB029 9%,
+        #74D3A7 13%,
+        #EEF2FF 18%,
+        #4366C9 23%,
+        #1E3576 26%,
+        #071E24 30%,
+        #0D0529 31%,
+        #11010A 32%,
+        #160518 33%,
+        #F45667 35%,
+        #FD3CA0 40%,
+        #FF9EBB 45%,
+        #FFDDE1 48%,
+        #FFF0F1 52%,
+        #FFC4FF 55%,
+        #DFBDFF 56%,
+        #5D91A2 60%,
+        #DD8736 63%,
+        #FFFAF9 67%,
+        #F1C6FF 69%,
+        #9FD9E6 72%,
+        #5EBACF 76%,
+        #B7CF8D 80%,
+        #EFDED8 85%,
+        #E83F42 93%,
+        #D6D329 95%,
+        #E4FC87 100%
+      )`,
+      backgroundAttachment: 'local',
+      backgroundSize: '100% 600vh',
+      backgroundRepeat: 'no-repeat'
+    }} className="main-page overflow-y-scroll h-screen">
+
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Education />
+      <Links />
+    </div>
+  ) 
+}
+
+export default App
---
+++ src/sections/experience.tsx
@@ -0,0 +1,11 @@
+// experience page
+
+function Experience() {
+    return (
+        <div className="h-dvh flex flex-col items-center justify-center snap-y"> 
+            <h1 className="text-white text-[20px] font-bold"> EXPERIENCE PLACEHOLDER </h1>
+        </div>
+    )
+}
+
+export default Experience
