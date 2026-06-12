545a9b912589ee469adf67881951fc446f13ea20
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-12 14:04:56 +1200

    fixed formatting to work on mobile experience page

--- src/sections/experience.tsx
+++ src/sections/experience.tsx
@@ -3,9 +3,57 @@ import ExperienceCard from '../components/experience-card.tsx';
 
 function Experience() {
     return (
-        <div className="h-dvh flex items-center justify-center snap-y p-[5%]"> 
-            <h1 className="text-white text-[15px] md:text-[20px] font-bold pr-[5%] md: pr-[10%]"> EXPERIENCE </h1>
-            <div className="experience-card-container flex flex-col justify-start bg-[#FFD0D0]/40 w-[100%] md:w-[65%] h-[80%] md:h-[90%] rounded-[50px] shadow-xl overflow-y-auto scroll-smooth p-[2%] gap-[5%]">
+        <div className="h-dvh 
+                        flex    
+                        flex-col
+                        items-center 
+                        justify-center 
+                        snap-y 
+                        p-[5%]
+                        gap-[5%]
+                        
+                        md:h-dvh 
+                        md:flex 
+                        md:flex-row
+                        md:items-center 
+                        md:justify-center 
+                        md:snap-y 
+                        md:p-[5%]"> 
+            <h1 className="text-white 
+                        align-center
+                        text-[15px] 
+                        font-bold 
+                        
+                        md:text-[20px] 
+                        md:pr-[5%]"> EXPERIENCE </h1>
+            <div className="experience-card-container 
+
+                            flex 
+                            flex-col 
+                            justify-start 
+                            bg-[#FFD0D0]/40 
+                            w-[100%]
+                            h-[80%] 
+                            rounded-[50px] 
+                            shadow-xl 
+                            overflow-y-auto 
+                            scroll-smooth 
+                            p-[2%] 
+                            gap-[5%]
+                            
+                            md:flex 
+                            md:flex-col 
+                            md:justify-start 
+                            md:bg-[#FFD0D0]/40 
+                            md:w-[100%]
+                            md:w-[65%] 
+                            md:h-[90%] 
+                            md:rounded-[50px] 
+                            md:shadow-xl 
+                            md:overflow-y-auto 
+                            md:scroll-smooth p-[2%] 
+                            md:gap-[5%]
+                            md:h-[90%] ">
                 <ExperienceCard />
                 <ExperienceCard />
                 <ExperienceCard />
