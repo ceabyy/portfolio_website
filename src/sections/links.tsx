c1c9ae6d8f8cdb1b3d7c775407132c3b6b61bf9d
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 14:05:50 +1200

    cleaned up layout of code in links tailwind css

--- src/sections/links.tsx
+++ src/sections/links.tsx
@@ -3,9 +3,34 @@ function Links() {
     return(
         <div className="h-dvh flex flex-col items-start justify-center snap-y pl-[20%]">
             <h1 className="text-[40px] font-bold text-white pb-[2%]"> LINKS </h1>
-                <a href = "https://github.com/ceabyy" target="_blank" className="text-[20px] font-light underline text-[#FED6D6] text-underline transition-colors duration-300 ease-in-out hover:text-white hover:font-bold"> GITHUB </a>
-                <a href = "https://www.linkedin.com/in/ceaby/" target="_blank" className="text-[20px] font-light underline text-[#FED6D6] text-underline transition-colors duration-300 hover:text-white hover:font-bold"> LINKEDIN </a>
-                <a href = "https://www.youtube.com/@ceaby." target="_blank" className="text-[20px] font-light underline text-[#FED6D6] text-underline transition-colors duration-300 hover:text-white hover:font-bold"> YOUTUBE </a>      
+                <a href = "https://github.com/ceabyy" target="_blank" className="text-[20px] 
+                                                                                font-light 
+                                                                                underline 
+                                                                                text-[#FED6D6] 
+                                                                                text-underline 
+                                                                                transition-colors 
+                                                                                duration-300 
+                                                                                ease-in-out 
+                                                                                hover:text-white 
+                                                                                hover:font-bold"> GITHUB </a>
+                <a href = "https://www.linkedin.com/in/ceaby/" target="_blank" className="text-[20px] 
+                                                                                        font-light 
+                                                                                        underline 
+                                                                                        text-[#FED6D6] 
+                                                                                        text-underline 
+                                                                                        transition-colors 
+                                                                                        duration-300
+                                                                                        hover:text-white 
+                                                                                        hover:font-bold"> LINKEDIN </a>
+                <a href = "https://www.youtube.com/@ceaby." target="_blank" className="text-[20px]
+                                                                                        font-light
+                                                                                        underline 
+                                                                                        text-[#FED6D6] 
+                                                                                        text-underline 
+                                                                                        transition-colors 
+                                                                                        duration-300 
+                                                                                        hover:text-white 
+                                                                                        hover:font-bold"> YOUTUBE </a>      
         </div>
     )
 }
