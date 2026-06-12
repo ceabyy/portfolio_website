11890cce404d05b523087073a8abe5ba772267e7
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 16:11:12 +1200

    added scroll snap for all pages

--- src/sections/education.tsx
+++ src/sections/education.tsx
@@ -3,7 +3,7 @@ import EducationCard from '../components/education-card.tsx';
 function Education() {
 
     return(
-        <div className="h-dvh flex flex-col items-start justify-center snap-y pl-[20%]">
+        <div className="h-dvh flex flex-col items-start justify-center pl-[20%]">
             <h1 className="text-[30px] text-white font-bold flex"> EDUCATION. </h1>
             <EducationCard
                 school="Tampere University"
