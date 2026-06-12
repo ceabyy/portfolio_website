44da8ebe90e16ecfd66f3dedfa9e09d2bcd09636
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 13:32:38 +1200

    added project cards and changes

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,18 +1,40 @@
+// experience cards for container in experience page
 
-type Props = {
-    title: string;
-    subtitle: string;
-    details: string;  /* replace with string[] later on */ 
-}
+// add props here later on
 
-
-function ProjectCard({ title, subtitle, details }: Props) {
+function ProjectCard() {
     return (
-        <article className="project-card">
-            <h2>{title}</h2>
-            <h3>{subtitle}</h3>
-            <p>{details}</p>
-        </article>
+        <div className="bg-[#FFE6F7] 
+                        flex 
+                        flex-shrink-0 
+                        flex-col 
+                        justify-start 
+                        h-full 
+                        w-[40%] 
+                        rounded-[50px] 
+                        p-[10%] md:p-[3%] 
+                        shadow-2xl 
+                        overflow-y-auto 
+
+                        md:overflow-y-hidden">
+            <p className="text-[20px]  
+                        text-[#C178B4] 
+                        font-bold 
+                        pb-[5%] 
+
+                        md:text-[25px]"> PROJECT </p>
+            <div className="bg-[#FFFFFF]
+                            text-[#AAAAAA] 
+                            h-[200%] 
+                            w-[100%]    
+                            rounded-[10px]
+                            flex
+                            flex-col
+                            items-center
+                            justify-center"> image placeholder </div>
+            <p className="text-[15px] md:text-[20px] text-[#C178B4] italic -mt-1 md:-mt-2 pt-[5%]"> Project Details </p>
+            <p className=" h-[90%] w-full text-[10px] md:text-[15px] text-[#CB76B1] font-light pt-[5%] md:pt-[1%] break-words"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada aliquam luctus. Fusce maximus tempor laoreet. Integer eget dolor nisl. Suspendisse ultrices sodales elementum. Quisque tempor est rutrum placerat pulvinar. Phasellus in enim vitae odio efficitur iaculis eget eu ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+        </div>
     )
 }
 
