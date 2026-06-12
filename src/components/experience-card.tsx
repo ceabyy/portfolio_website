08bc175d28104b96348e4f140537ae7c393e438f
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-12 12:54:34 +1200

    adjusted padding in experience cards

--- src/components/experience-card.tsx
+++ src/components/experience-card.tsx
@@ -4,10 +4,10 @@
 
 function ExperienceCard() {
     return (
-        <div className="bg-[#FFDADB] flex flex-shrink-0 flex-col justify-start h-[30%] w-full rounded-[50px] p-[15%] md:p-[5%] shadow-xl overflow-y-auto md:overflow-y-hidden">
-            <p className="text-[25px] text-[#B73854] font-bold "> COMPANY </p>
-            <p className="text-[20px] text-[#B73854] font-light italic -mt-3"> June 2026 - Present</p>
-            <p className=" h-[90%] w-full text-[10px] md:text-[15px] text-[#B73854] font-light pt-[1%] break-words"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada aliquam luctus. Fusce maximus tempor laoreet. Integer eget dolor nisl. Suspendisse ultrices sodales elementum. Quisque tempor est rutrum placerat pulvinar. Phasellus in enim vitae odio efficitur iaculis eget eu ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+        <div className="bg-[#FFDADB] flex flex-shrink-0 flex-col justify-start h-[30%] w-full rounded-[50px] p-[10%] md:p-[5%] shadow-xl overflow-y-auto md:overflow-y-hidden">
+            <p className="text-[20px] md:text-[25px] text-[#B73854] font-bold "> COMPANY </p>
+            <p className="text-[15px] md:text-[20px] text-[#B73854] font-light italic -mt-1 md:-mt-3"> June 2026 - Present</p>
+            <p className=" h-[90%] w-full text-[10px] md:text-[15px] text-[#B73854] font-light pt-[5%] md:pt-[1%] break-words"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada aliquam luctus. Fusce maximus tempor laoreet. Integer eget dolor nisl. Suspendisse ultrices sodales elementum. Quisque tempor est rutrum placerat pulvinar. Phasellus in enim vitae odio efficitur iaculis eget eu ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
         </div>
     )
 }
