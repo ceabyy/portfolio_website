1be539298997679e04d877b7870074e1f31a894e
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 12:48:04 +1200

    added experience card and base layout for it

---
+++ src/components/experience-card.tsx
@@ -0,0 +1,19 @@
+// experience cards for container in experience page
+
+type Props = {
+    title: string;
+    dates: string;
+    information: string; /* this can be turned into bullet points later on -- map function */
+}
+
+function ExperienceCard() {
+    return (
+        <div className="bg-[#FFDADB] flex flex-shrink-0 flex-col justify-center h-[30%] w-full rounded-[50px] p-[15%] md:p-[5%] shadow-xl overflow-y-auto md:overflow-y-hidden">
+            <p className="text-[25px] text-[#B73854] font-bold "> COMPANY </p>
+            <p className="text-[20px] text-[#B73854] font-light italic -mt-3"> June 2026 - Present</p>
+            <p className=" h-[90%] w-full text-[10px] md:text-[15px] text-[#B73854] font-light pt-[1%] break-words"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada aliquam luctus. Fusce maximus tempor laoreet. Integer eget dolor nisl. Suspendisse ultrices sodales elementum. Quisque tempor est rutrum placerat pulvinar. Phasellus in enim vitae odio efficitur iaculis eget eu ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
+        </div>
+    )
+}
+
+export default ExperienceCard
--- src/sections/experience.tsx
+++ src/sections/experience.tsx
@@ -1,9 +1,18 @@
 // experience page
+import ExperienceCard from '../components/experience-card.tsx';
 
 function Experience() {
     return (
-        <div className="h-dvh flex flex-col items-center justify-center snap-y"> 
-            <h1 className="text-white text-[20px] font-bold"> EXPERIENCE PLACEHOLDER </h1>
+        <div className="h-dvh flex items-center justify-center snap-y p-[5%]"> 
+            <h1 className="text-white text-[20px] font-bold pr-[5%]"> EXPERIENCE </h1>
+            <div className="experience-card-container flex flex-col justify-start bg-[#FFD0D0]/40 w-[65%] h-[90%] rounded-[50px] shadow-xl overflow-y-auto scroll-smooth p-[2%] gap-[5%]">
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+                <ExperienceCard />
+            </div>
         </div>
     )
 }
