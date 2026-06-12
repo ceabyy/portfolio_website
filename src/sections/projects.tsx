5d00114ed3384abe9f2137100fb74948638ecf2d
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 14:20:48 +1200

    changed text content to include full stops

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -4,9 +4,9 @@ import ProjectCard from '../components/project-card.tsx';
 function Projects() {
     return (
         <div className = "h-dvh flex flex-col items-center justify-center snap-y">
-            <h1 className="text-[20px] md:text-[25px] font-bold text-[#904C7E] pb-[2%]"> PROJECTS </h1>
+            <h1 className="text-[20px] md:text-[25px] font-bold text-[#904C7E] pb-[2%]"> PROJECTS. </h1>
             <div className="project-card-container 
-            
+
                             flex 
                             flex-col
                             items-center 
