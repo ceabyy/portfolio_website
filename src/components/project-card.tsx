4ae61322b8cfeba6dded674139066aac051834c3
ceabyy <ceabyfernandez@gmail.com>
2026-06-12 14:06:50 +1200

    cleaned up tailwind css code and changes for mobile friendliness

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -9,13 +9,23 @@ function ProjectCard() {
                         flex-shrink-0 
                         flex-col 
                         justify-start 
-                        h-full 
-                        w-[40%] 
+                        h-[80%] 
+                        w-[90%] 
                         rounded-[50px] 
-                        p-[10%] md:p-[3%] 
+                        p-[10%]
                         shadow-2xl 
                         overflow-y-auto 
 
+                        md:bg-[#FFE6F7] 
+                        md:flex 
+                        md:flex-shrink-0 
+                        md:flex-col 
+                        md:justify-start 
+                        md:h-full 
+                        md:w-[40%] 
+                        md:rounded-[50px] 
+                        md:shadow-2xl 
+                        md:p-[3%] 
                         md:overflow-y-hidden">
             <p className="text-[20px]  
                         text-[#C178B4] 
@@ -31,7 +41,8 @@ function ProjectCard() {
                             flex
                             flex-col
                             items-center
-                            justify-center"> image placeholder </div>
+                            justify-center
+                            shadow-lg"> image placeholder </div>
             <p className="text-[15px] md:text-[20px] text-[#C178B4] italic -mt-1 md:-mt-2 pt-[5%]"> Project Details </p>
             <p className=" h-[90%] w-full text-[10px] md:text-[15px] text-[#CB76B1] font-light pt-[5%] md:pt-[1%] break-words"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam malesuada aliquam luctus. Fusce maximus tempor laoreet. Integer eget dolor nisl. Suspendisse ultrices sodales elementum. Quisque tempor est rutrum placerat pulvinar. Phasellus in enim vitae odio efficitur iaculis eget eu ex. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
         </div>
