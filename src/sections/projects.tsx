f99c7dcf0f728cf93e44e8d7381c5cb881fab633
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-12 13:32:38 +1200

    added project cards and changes

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,11 +1,28 @@
 // PROJECTS SECTION /PAGE
+import ProjectCard from '../components/project-card.tsx';
 
 function Projects() {
     return (
         <div className = "h-dvh flex flex-col items-center justify-center snap-y">
-            <h1 className="text-[30px] font-bold text-[#904C7E] pb-[2%]"> PROJECTS </h1>
-            <div className="project-card-container flex flex-row items-center justify-center w-[80%] h-[80%] bg-[#FFD0D0]/35 rounded-[50px] shadow-xl">
-                <h1 className="text-[30px] text-white opacity-100"> Project Container </h1>
+            <h1 className="text-[20px] md:text-[25px] font-bold text-[#904C7E] pb-[2%]"> PROJECTS </h1>
+            <div className="project-card-container 
+                            flex 
+                            flex-row 
+                            items-center 
+                            justify-start 
+                            w-[80%] 
+                            h-[80%] 
+                            bg-[#FFD0D0]/35 
+                            rounded-[50px] 
+                            shadow-xl 
+                            p-[2%] 
+                            gap-[2%] 
+                            overflow-x-scroll">
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
+                <ProjectCard />
                 {/*  map function for each project card below */}
             </div>
         </div>
