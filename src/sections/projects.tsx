86f7532bf244d8c5323d68575945e6c29f6eeda5
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-12 14:06:50 +1200

    cleaned up tailwind css code and changes for mobile friendliness

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -6,18 +6,33 @@ function Projects() {
         <div className = "h-dvh flex flex-col items-center justify-center snap-y">
             <h1 className="text-[20px] md:text-[25px] font-bold text-[#904C7E] pb-[2%]"> PROJECTS </h1>
             <div className="project-card-container 
+            
                             flex 
-                            flex-row 
+                            flex-col
                             items-center 
                             justify-start 
-                            w-[80%] 
+                            w-[90%] 
                             h-[80%] 
                             bg-[#FFD0D0]/35 
                             rounded-[50px] 
                             shadow-xl 
-                            p-[2%] 
-                            gap-[2%] 
-                            overflow-x-scroll">
+                            p-[2%]
+                            pt-[5%] 
+                            gap-[5%] 
+                            overflow-y-scroll
+                            
+                            md:flex 
+                            md:flex-row 
+                            md:items-center 
+                            md:justify-start 
+                            md:w-[80%] 
+                            md:h-[80%] 
+                            md:bg-[#FFD0D0]/35 
+                            md:rounded-[50px] 
+                            md:shadow-xl 
+                            md:p-[2%] 
+                            md:gap-[2%] 
+                            md:overflow-x-scroll">
                 <ProjectCard />
                 <ProjectCard />
                 <ProjectCard />
