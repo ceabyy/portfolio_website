b66050ba2e3362fe89bdb27e71682e10c8ad92e2
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-12 14:06:20 +1200

    cleaned up layout on website, formatted tailwind css

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -12,7 +12,7 @@ function About() {
         { id:"Object-Oriented Programming", name:"Object-Oriented Programming" },
         { id:"Supabase", name:"Supabase" },
         { id:"Next.js", name:"Next.js" },
-        { id:"CSS", name:"CSS" },
+        { id:"Data Structures & Algorithms", name:"Data Structures & Algorithms" },
         { id:"CSS", name:"CSS" },
         { id:"CSS", name:"CSS" },
         { id:"CSS", name:"CSS" },
@@ -20,11 +20,11 @@ function About() {
     ]
 
     return(
-        <div className="h-dvh grid grid-cols-1 md:grid-cols-[2fr_2.5fr] items-center justify-center snap-y">
+        <div className="h-dvh flex flex-col md:grid-cols-[2fr_2.5fr] items-center justify-center snap-y">
             {/* column 1 */}
-            <section className = "flex flex-col items-start p-[20%]"> 
-                <h1 className="text-white text-[20px] text-center font-bold pb-[3%]"> SKILLS </h1>
-                <section className="flex flex-wrap gap-[10px] gap-y-[5px]"> 
+            <section className = "h-[50%] flex flex-col items-start pl-[20%] pr-[20%]"> 
+                <h1 className="text-[#1E3576] text-[20px] text-center font-bold pb-[3%]"> SKILLS </h1>
+                <section className="flex flex-wrap gap-[10px] gap-y-[10px]"> 
 
                     {/* map function */}
                     {skills.map((skill) => (
@@ -35,9 +35,9 @@ function About() {
             </section>
 
             {/* column 2 */}
-            <section className="flex flex-col items-start pl-[20%] md:pl-[5%] pr-[15%]">
+            <section className="h-[50%] flex flex-col items-start pl-[20%] pr-[20%]">
                 <h1 className="text-white text-[20px] text-center font-bold pb-[3%]"> ABOUT </h1>
-                <p className="text-white font-extralight"> Hi! My name is Ceaby. I'd say I'm very attentive, curious, self-motivated and thrive as a team leader or member. I'm a Bachelor of Science (Technology) graduate, having majored in signal processing and machine learning (ML), with a further educational background in computer science, ML and artificial intelligence (AI).</p>
+                <p className="text-white font-extralight "> Hi! My name is Ceaby. I'd say I'm very attentive, curious, self-motivated and thrive as a team leader or member. I'm a Bachelor of Science (Technology) graduate, having majored in signal processing and machine learning (ML), with a further educational background in computer science, ML and artificial intelligence (AI).</p>
             </section> 
 
             {/* potential column 3 for ascii of my photo */}
