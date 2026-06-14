b6f07a48dabc378a559899f37a1b910af04fe5b2
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-06-14 12:37:08 +1200

    refactored code

--- src/components/skills-bubble.tsx
+++ src/components/skills-bubble.tsx
@@ -6,8 +6,13 @@ type Props = {
 
 function SkillsBubble({ name }: Props) {
     return (
-        <div className="bg-[#F5F3FF] drop-shadow-sm rounded-[15px]">
-            <p className="pl-3 pr-3 pt-1 pb-1"> { name } </p>
+        <div className="bg-[#F5F3FF] 
+                        drop-shadow-sm 
+                        rounded-[15px]">
+            <p className="pl-3 
+                        pr-3 
+                        pt-1 
+                        pb-1"> { name } </p>
         </div>
     )
 }
