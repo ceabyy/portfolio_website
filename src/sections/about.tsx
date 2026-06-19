49cfda80c2d727702ebbadbbd9add631213b4919
Ceaby Fernandez <ceabyfernandez@gmail.com>
2026-06-19 23:28:08 +1200

    installed framer motion, added motion to hero and about section

--- src/sections/about.tsx
+++ src/sections/about.tsx
@@ -1,4 +1,37 @@
 import SkillBubble from "../components/skills-bubble.tsx";
+import { motion, type Variants } from 'framer-motion';
+
+// for motion handling, i.e. fade in and move up movement
+
+const container: Variants = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.15
+        }
+    }
+};
+
+const NestedContainer: Variants = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.03
+        }
+    }
+};
+
+const item: Variants = {
+    hidden: { opacity: 0, y: 40 },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.6,
+            ease: "easeInOut"
+        }
+    }
+}
 
 function About() {
 
@@ -20,17 +53,22 @@ function About() {
     ]
 
     return(
-        <div className="h-dvh 
-                        flex 
-                        flex-col 
-                        items-center 
-                        justify-center 
-                        snap-y 
-                        gap-[5%]
-                        
-                        md:flex-row">
+        <motion.div variants={ container }
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: true, amount: 0.3 }}
+                    className="h-dvh 
+                    flex 
+                    flex-col 
+                    items-center 
+                    justify-center 
+                    snap-y 
+                    gap-[5%]
+                    
+                    md:flex-row">
             {/* column 1 */}
-            <section className="h-[50%] 
+            <motion.section variants={ item }
+                            className="h-[50%] 
                                 flex 
                                 flex-col 
                                 items-start 
@@ -49,21 +87,28 @@ function About() {
                                 pb-[3%]
                                 
                                 md:text-white text-[20px] "> SKILLS. </h1>
-                <section className="flex 
+                <motion.section variants={ NestedContainer}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: true, amount: 0.3 }}
+                                className="flex 
                                     flex-wrap 
                                     gap-[10px] 
                                     gap-y-[10px]"> 
 
                     {/* map function */}
                     {skills.map((skill) => (
-                        <SkillBubble key={skill.id} name={skill.name} /> 
+                        <motion.div key={ skill.id } variants={ item }>
+                            <SkillBubble key={skill.id} name={skill.name} /> 
+                        </motion.div>
                     ))}
 
-                </section> 
-            </section>
+                </motion.section> 
+            </motion.section>
 
             {/* column 2 */}
-            <section className="h-[50%] 
+            <motion.section variants={ container }
+                            className="h-[50%] 
                                 flex 
                                 flex-col 
                                 items-start 
@@ -76,19 +121,18 @@ function About() {
                                 md:h-full 
                                 md:w-[60%]">
 
-                <h1 className="text-white 
+                <motion.h1 variants={ item } className="text-white 
                                 text-[20px] 
                                 text-center 
                                 font-bold 
-                                pb-[3%]"> ABOUT. </h1>
-                <p className="text-white 
+                                pb-[3%]"> ABOUT. </motion.h1>
+                <motion.p variants={ item } className="text-white 
                             font-extralight 
-                            md:text-[20px] "> Hi! My name is Ceaby. I'd say I'm very attentive, curious, self-motivated and thrive as a team leader or member. I'm a Bachelor of Science (Technology) graduate, having majored in signal processing and machine learning (ML), with a further educational background in computer science, ML and artificial intelligence (AI).</p>
+                            md:text-[20px] "> Hi! My name is Ceaby. I'd say I'm very attentive, curious, self-motivated and thrive as a team leader or member. I'm a Bachelor of Science (Technology) graduate, having majored in signal processing and machine learning (ML), with a further educational background in computer science, ML and artificial intelligence (AI).</motion.p>
             
-            </section> 
-
+            </motion.section> 
             {/* potential column 3 for ascii of my photo */}
-        </div>
+        </motion.div>
     )
 }
 
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,8 +1,36 @@
 
 // component to handle the hero section, with name and "portfolio."
+import { motion, type Variants } from 'framer-motion';
+
+const container: Variants = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.1
+        }
+    }
+};
+
+const item: Variants = {
+    hidden: { opacity: 0, y: 20 },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.6,
+            ease: "easeInOut"
+        }
+    }
+}
+
 function Hero() {
     return (
-        <div className="h-dvh 
+        <motion.div 
+            variants={ container }
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.3}}
+            className="h-dvh 
                         flex
                         flex-col 
                         items-start 
@@ -10,20 +38,20 @@ function Hero() {
                         snap-y 
                         pl-[15%]">
 
-            <h1 className="text-[20px] 
+            <motion.h1 variants={item} className="text-[20px] 
                             text-white
 
-                            md:text-[40px]"> CEABY KIAN FERNANDEZ </h1>
+                            md:text-[40px]"> CEABY KIAN FERNANDEZ </motion.h1>
                             
-            <h1 className="text-[20px] 
+            <motion.h1 variants={item} className="text-[20px] 
                             md:text-[40px] 
                             font-bold 
                             underline 
                             text-white
                             text-underline 
 
-                            md:-mt-5 "> portfolio.</h1>
-        </div>
+                            md:-mt-5 "> portfolio.</motion.h1>
+        </motion.div>
     )
 }
 
